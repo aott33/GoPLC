330082cfe9ebbfc9928b5a7243749ff1c0e1c70a
aott33 <ott.andrew33@gmail.com>
2025-12-18 15:48:21 -0800

    dev - create config struct, research into option struct fields and struct field default values

---
+++ internal/config/types.go
@@ -0,0 +1,41 @@
+package config
+
+type Config struct {
+	LogLevel		string
+	Sources			[]Sources
+	Variables		[]Variables
+}
+
+type Sources struct {
+	Name			string `yaml:"name"`
+	Type			string `yaml:"type"`		
+}
+
+type ModbusTCPConfig struct {
+	Host			string `yaml:"host"`
+	Port			int    `yaml:"port"`
+	UnitID			int    `yaml:"unitId"`
+	Timeout			string `yaml:"timeout"`
+	PollInterval 	string `yaml:"pollInterval"`
+	RetryInterval	string `yaml:"retryInterval"`
+	ByteOrder		string `yaml:"byteOrder"`
+	WordOrder		string `yaml:"wordOrder"`
+}
+
+type ModbusRTUConfig struct {
+	Device       	string `yaml:"device"`
+	BaudRate     	int    `yaml:"baudRate"`
+	DataBits     	int    `yaml:"dataBits"`
+	Parity       	string `yaml:"parity"`
+	StopBits     	int    `yaml:"stopBits"`
+	UnitID       	int    `yaml:"unitId"`
+	Timeout      	string `yaml:"timeout"`
+	PollInterval 	string `yaml:"pollInterval"`
+}
+
+type Variables struct {
+	Name			string `yaml:"name"`
+	Source			string `yaml:"source"`
+	DataType		string `yaml:"dataType"`
+	Tags			[]string `yaml:"tags"`
+}
